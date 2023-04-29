bf980e5b553f26d50269cbdd18ca7fae4b5d0d25
V14dik <cherenkevich.vlad@gmail.com>
2023-04-29 20:16:38 +0300

    Add deed endpoints

--- server/src/deed/deed.controller.ts
+++ server/src/deed/deed.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Post, Query, Delete, Patch} from '@nestjs/common';
+import {Deed} from "./deed.schema";
+import {CreateDeedDto} from "./create-deed.dto";
+import {DeedService} from "./deed.service";
 
 @Controller('deed')
-export class DeedController {}
+export class DeedController {
+
+    constructor(private deedService: DeedService) {}
+
+    @Get()
+    async getDeed(@Query() query): Promise<Deed> {
+        return this.deedService.findDeed(query.deedId);
+    }
+
+    @Post()
+    async createDeed(@Body() dto: CreateDeedDto){
+        return this.deedService.create(dto);
+    }
+
+    @Delete()
+    async deleteDeed(@Query() {deedId}) {
+        await this.deedService.delete(deedId);
+    }
+
+    @Patch()
+    async editDeed(@Query() {deedId}, @Body() deed) {
+        return this.deedService.edit(deedId, deed);
+    }
+
+    @Get("list")
+    async getUserDeeds(@Query() {userId}) {
+        return this.deedService.findUserDeeds(userId);
+    }
+}
--- server/src/deed/deed.service.ts
+++ server/src/deed/deed.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/mongoose";
+import {Deed, DeedDocument} from "./deed.schema";
+import {Model} from "mongoose";
+import {CreateDeedDto} from "./create-deed.dto";
 
 @Injectable()
-export class DeedService {}
+export class DeedService {
+    constructor(@InjectModel(Deed.name) private deedModel:Model<DeedDocument>) {}
+
+    async create(deed: CreateDeedDto) {
+        const newDeed = await this.deedModel.create(deed);
+        return newDeed;
+    }
+
+    async findDeed(deedId) {
+        return this.deedModel.findById(deedId);
+    }
+
+    async findUserDeeds(userId) {
+        return this.deedModel.find({user: userId});
+    }
+
+    async delete(id) {
+        await this.deedModel.findByIdAndDelete(id);
+    }
+
+    async edit(id, deed) {
+        const newDeed = await this.deedModel.findByIdAndUpdate(id, {...deed}, {new: true});
+        return newDeed;
+    }
+}
